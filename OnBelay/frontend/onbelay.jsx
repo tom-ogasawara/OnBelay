ee75c62cabda548f9aa1a037ff32a7edd162a67f
Tom Ogasawara <tom.ogasawara@gmail.com>
2017-01-10 15:50:33 -0800

    Update entry file to account for current user

--- OnBelay/frontend/onbelay.jsx
+++ OnBelay/frontend/onbelay.jsx
@@ -1,9 +1,20 @@
+//React
+import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store';
+//Components
 import Root from './components/root';
+import configureStore from './store/store';
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
+
   const root = document.getElementById('root');
-  ReactDOM.render(<Root store={ store }/>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
