fdaa927bb4516938d126558c6421c1c63ed4245d
Tom Ogasawara <tom.ogasawara@gmail.com>
2017-01-10 12:05:28 -0800

    Create static pages controller and root view

--- OnBelay/config/environment.rb
+++ OnBelay/config/environment.rb
@@ -3,8 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# config/environments/production.rb
-config.serve_static_files = true
-# ...
-config.log_level = :debug
