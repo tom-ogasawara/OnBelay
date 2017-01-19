2354e8d40f6e8776252e9e397b34bbdb71ac6f81
Tom Ogasawara <tom.ogasawara@gmail.com>
2017-01-19 12:57:09 -0800

    Edit conversation Actions

--- OnBelay/frontend/actions/conversation_actions.js
+++ OnBelay/frontend/actions/conversation_actions.js
@@ -1,25 +1,25 @@
 import * as APIUtil from '../util/conversation_api_util';
 
-export const RECEIVE_CURRENT_CONVERSATION = "RECEIVE_CURRENT_CONVERSATION";
 export const RECEIVE_CONVERSATIONS = "RECEIVE_CONVERSATIONS";
+export const RECEIVE_CURRENT_CONVERSATION = "RECEIVE_CURRENT_CONVERSATION";
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+export const fetchConversations = () => {
+  return (dispatch) => {
+    return APIUtil.fetchConversations()
+    .then(
+      (conversations) => dispatch(receiveConversations(conversations)),
+      (errors) => dispatch(receiveErrors(errors.responseJSON))
+    );
+  };
+};
 
 export const fetchSingleConversation = (thread_id) => {
   return (dispatch) => {
     return APIUtil.fetchSingleConversation(thread_id)
       .then(
         (conversation) => dispatch(receiveCurrentConversation(conversation)),
-        (errors) => dispatch(receiveErrors(errors.responseJSON))
-      );
-  };
-};
-
-export const fetchConversations = () => {
-  return (dispatch) => {
-    return APIUtil.fetchConversations()
-      .then(
-        (conversations) => dispatch(receiveConversations(conversations)),
         (errors) => dispatch(receiveErrors(errors.responseJSON))
       );
   };
