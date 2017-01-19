2fba5dd059f3a78f98a833ca856fbdc47a435052
Tom Ogasawara <tom.ogasawara@gmail.com>
2017-01-19 15:10:24 -0800

    Update distance params

--- OnBelay/frontend/components/browse/matches.jsx
+++ OnBelay/frontend/components/browse/matches.jsx
@@ -239,9 +239,9 @@ class Matches extends React.Component {
     return (
       <select className="sort-dropdown" onChange={ this.handleDistance }>
         <option value="2500">Any</option>
-        <option value="2">2 miles</option>
         <option value="5">5 miles</option>
         <option value="10">10 miles</option>
+        <option value="25">25 miles</option>
         <option value="50">50 miles</option>
         <option value="100">100 miles</option>
       </select>
